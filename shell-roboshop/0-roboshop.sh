54e730ff9042ab045e7f9f351632a86c189c14cd
param <param@gmail.com>
2026-02-15 12:49:02 +0530

    shell

--- shell-roboshop/0-roboshop.sh
+++ shell-roboshop/0-roboshop.sh
@@ -57,29 +57,7 @@ do
             }
         ]
     }
-    
-
-    echo "record updated for $instance"    
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Updating record",
-        "Changes": [
-            {
-            "Action": "UPSERT",
-            "ResourceRecordSet": {
-                "Name": "'$RECORD_NAME'",
-                "Type": "A",
-                "TTL": 1,
-                "ResourceRecords": [
-                {
-                    "Value": "'$IP'"
-                }
-                ]
-            }  
-        ]
-    } 
+    '
 
     echo "record updated for $instance"
 
