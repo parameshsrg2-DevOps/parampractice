989934d0cce15a6baecf6cd8bf2728a8b3872756
param <param@gmail.com>
2026-02-15 13:04:04 +0530

    shell

--- shell-roboshop/00-robo.sh
+++ shell-roboshop/00-robo.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
+SG_ID="sg-0f57b313ae45eaff0" # replace with your ID
 AMI_ID="ami-0220d79f3f480ecf5"
-SG_ID="sg-0f57b313ae45eaff0"
+#ZONE_ID="Z05005173GDIZPB1JV6KA"
+#DOMAIN_NAME="farm2home.shop"
 
 for instance in $@
 do
-    echo "Creating instance: $instance"
-    
     INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
-    --output text)
+    --output text )
 
-    echo "Instance ID: $INSTANCE_ID"
-    
-    # Get the IP address
-    IP=$( aws ec2 describe-instances \
-    --instance-ids $INSTANCE_ID \
-    --query 'Reservations[0].Instances[0].PublicIpAddress' \
-    --output text)
-    
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME" # farm2home.shop
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.farm2home.shop
+    fi
+
     echo "IP Address: $IP"
+
 done
