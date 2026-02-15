26597c4980fc445d3d3a0ceb50d62d48ce87d72f
param <param@gmail.com>
2026-02-15 12:40:49 +0530

    shell

---
+++ shell-roboshop/0-roboshop.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-0f57b313ae45eaff0"
+ZONE_ID="Z05005173GDIZPB1JV6KA"
+DOMAIN_NAME="farm2home.shop"
+
+
+for instance in $@
+do
+    INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
+
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" # farm2home.shop
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.farm2home.shop
+
+    fi
+    echo "IP Address:$IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+    
+
+    echo "record updated for $instance"    
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+    '
+
+    echo "record updated for $instance"
+
+done
---
+++ shell/3-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+START_TIME=$(date +%Y-%m-%d_%H:%M:%S) 
+
+echo "Script executed at: $START_TIME"
+
+sleep 10
+
+END_TIME=$(date +%Y-%m-%d_%H:%M:%S)
+
+echo "Script executed in: $END_TIME seconds"
+
+# Calculate duration
+TOTAL_TIME=$($END_TIME - $START_TIME)
+
+echo "Script executed in: $TOTAL_TIME"
