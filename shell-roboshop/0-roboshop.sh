fbc1ca1ca05a28e2825c5a07f7a2854c67222b32
param <param@gmail.com>
2026-02-15 13:12:46 +0530

    shell

--- shell-roboshop/0-roboshop.sh
+++ shell-roboshop/0-roboshop.sh
@@ -2,19 +2,20 @@
 
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-0f57b313ae45eaff0"
+INSTANCE_TYPE="t3.micro"
 #ZONE_ID="Z05005173GDIZPB1JV6KA"
 #DOMAIN_NAME="farm2home.shop"
 
 for instance in $@
 do
-    INSTANCE_ID=$( aws ec2 run-instances \
+   echo "Launching EC2 instance..."
+    INSTANCE_ID=$(aws ec2 run-instances \
     --image-id $AMI_ID \
-    --instance-type "t3.micro" \
+    --instance-type $INSTANCE_TYPE \
     --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
     --output text)
 
-    echo "IP Address: $IP"
+  echo "Instance ID: $INSTANCE_ID"
 
 done
